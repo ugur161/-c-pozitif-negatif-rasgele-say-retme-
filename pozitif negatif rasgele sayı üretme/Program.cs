094bfb5f3d5240bd62570d23afc3f87d08221acc
UĞURCAN <UURCAN@LAPTOP-SAB3RQO3>
2023-12-19 22:22:01 +0300

    Proje dosyası ekle.

---
+++ pozitif negatif rasgele sayı üretme/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pozitif_negatif_rasgele_sayı_üretme
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { Random rasgele=new Random();
+            int üretilensayı;
+            int ps = 0, ns = 0;
+            for(int i = 0; i<50;i++)
+            {
+                üretilensayı = rasgele.Next(-100,100);
+                Console.WriteLine(üretilensayı+",");
+                if (üretilensayı > 0)
+                {
+                    ps++; 
+                }
+                else if (üretilensayı < 0) 
+                {
+                    ns++;
+                }
+                Console.WriteLine($"\n50 adet sayıdan {ps}tanesi pozitif, {ns} tanesi negatiftir.");
+                Console.ReadKey();
+
+
+
+
+
+            }
+        }
+    }
+}
